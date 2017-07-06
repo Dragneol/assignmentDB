3af7e65706d91c56bbf67bf1d234e9f64e6983b4
Dragneol <DuongPTHSE62871@fpt.edu.vn>
2017-07-06 14:54:50 +0700

    update 7/6/2017

---
+++ travel.com.vn.sql
@@ -0,0 +1,8 @@
+USE [master]
+GO
+
+/****** Object:  Database [travel.com.vn]    Script Date: 07/06/2017 13:17:52 ******/
+IF  EXISTS (SELECT name FROM sys.databases WHERE name = N'travel.com.vn')
+DROP DATABASE [travel.com.vn]
+GO
+
