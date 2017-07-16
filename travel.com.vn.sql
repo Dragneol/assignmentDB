573fee3df89ba867dc6b602a4facd6e6ad4e5f95
phqb <o0o.baoquocphan.o0o@gmail.com>
2017-07-16 17:08:52 +0700

    Thêm dữ liệu mẫu

--- travel.com.vn.sql
+++
@@ -1,8 +0,0 @@
-USE [master]
-GO
-
-/****** Object:  Database [travel.com.vn]    Script Date: 07/06/2017 13:17:52 ******/
-IF  EXISTS (SELECT name FROM sys.databases WHERE name = N'travel.com.vn')
-DROP DATABASE [travel.com.vn]
-GO
-
